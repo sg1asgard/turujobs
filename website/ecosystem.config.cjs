12d70ca761c3c6c09fcbeeb576f3916215204792
z <zeki@globaldigitalplatforms.com>
2024-09-01 21:39:20 +0300

    kind of reinternalize db a bit

--- website/ecosystem.config.cjs
+++ website/ecosystem.config.cjs
@@ -5,7 +5,10 @@ module.exports = {
       port: '3300',
       exec_mode: 'cluster',
       instances: 1,
-      script: './.output/server/index.mjs'
+      script: './.output/server/index.mjs',
+      env: {
+        NUXT_DB_JSON_PATH: '../database/db.json'
+      }
     }
   ]
 }
